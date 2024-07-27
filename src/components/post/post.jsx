576f5cb88ada03f0a39d96b5f5711a1b76afe477
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2024-07-27 16:56:43 +0200

    Fix comment text persisting after submit

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -7,6 +7,7 @@ import { format } from "date-fns"
 function CommentForm ({ postid }) {
     const navigate = useNavigate()
     const [comment, setComment] = useState({ postid: postid })
+    console.log(comment)
 
     function formUpdate(e) {
         
@@ -36,6 +37,7 @@ function CommentForm ({ postid }) {
                 console.log(res)
                 alert("Something is wrong with the data, check console")
             } else {
+                setComment({ postid: postid, text: ""})
                 navigate(`/${postid}`)
             }
         } catch (err) {
@@ -45,10 +47,10 @@ function CommentForm ({ postid }) {
     }
 
     return (
-        <form onSubmit={formSubmit}>
+        <form onSubmit={formSubmit} className={style.comment}>
             <div className={style.divForm}>
                 <label htmlFor="comment">Your Comment</label>
-                <textarea onChange={formUpdate} name="comment" id="comment" value={comment.text}></textarea>
+                <textarea onChange={formUpdate} name="comment" id="comment" value={comment.text} />
                 <input type="hidden" name="postid" id="postid" value={comment.post}/>
             </div>
             <button type="submit">Submit</button>
@@ -67,15 +69,15 @@ export default function Post () {
     if (user) {
         return (
             <div className={style.postFeed}>
-                <div>
+                <div className={style.postHolder}>
                     <h1>{post.title}</h1>
-                    <p>posted on {format(post.timeStamp, 'MMMM do')} by {post.user.username}</p>
+                    <p className={style.serviceText}>posted on {format(post.timeStamp, 'MMMM do')} by {post.user.username}</p>
                     <p>{post.text}</p>
                     <CommentForm postid={post._id} />
                     {comments.map(comment => {
                         return (
-                            <div key={comment.id}>
-                                <p>On {format(comment.timeStamp, 'MMMM do')} {comment.user.username} wrote</p>
+                            <div key={comment.id} className={style.comment}>
+                                <p className={style.serviceText}>On {format(comment.timeStamp, 'MMMM do')} {comment.user.username} wrote</p>
                                 <p>{comment.text}</p>
                             </div>
                         )
@@ -86,14 +88,14 @@ export default function Post () {
     } else {
         return (
             <div className={style.postFeed}>
-                <div>
+                <div className={style.postHolder}>
                     <h1>{post.title}</h1>
-                    <p>posted on {format(post.timeStamp, 'MMMM do')} by {post.user.username}</p>
+                    <p className={style.serviceText}>posted on {format(post.timeStamp, 'MMMM do')} by {post.user.username}</p>
                     <p>{post.text}</p>
                     {comments.map(comment => {
                         return (
-                            <div key={comment.id}>
-                                <p>On {format(comment.timeStamp, 'MMMM do')} {comment.user.username} wrote</p>
+                            <div key={comment.id} className={style.comment}>
+                                <p className={style.serviceText}>On {format(comment.timeStamp, 'MMMM do')} {comment.user.username} wrote</p>
                                 <p>{comment.text}</p>
                             </div>
                         )
