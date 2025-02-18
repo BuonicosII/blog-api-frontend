e02f81ed2edea8dcf915e50d53feef4aeef0d844
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2025-02-18 21:04:00 +0100

    Refactor react app to use psql

--- src/components/create-post/create-post.jsx
+++ src/components/create-post/create-post.jsx
@@ -1,81 +1,97 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import style from "./create-post.module.css"
-import PropTypes from 'prop-types'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "./create-post.module.css";
+import PropTypes from "prop-types";
 
-export default function CreatePost ( { postToEdit }) {
+export default function CreatePost({ postToEdit }) {
+  const [post, setPost] = useState(
+    postToEdit ? postToEdit : { published: false }
+  );
+  const navigate = useNavigate();
 
-    const [post, setPost] = useState( postToEdit ? postToEdit : {published: false})
-    const navigate = useNavigate()
+  function formUpdate(e) {
+    e.preventDefault();
 
-    function formUpdate(e) {
-        
-        e.preventDefault()
+    const newPost = {
+      ...post,
+      title: document.querySelector("#title").value,
+      text: document.querySelector("#text").value,
+    };
+    setPost(newPost);
+  }
 
-        const newPost = {
-            ...post,
-            title: document.querySelector("#title").value,
-            text: document.querySelector("#text").value,
-        }
-        setPost(newPost)
+  function checkUpdate() {
+    const newPost = {
+      ...post,
+      published: !post.published,
+    };
+    setPost(newPost);
+  }
 
+  async function formSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const json = await fetch("http://localhost:3000/posts", {
+        method: postToEdit ? "PUT" : "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+        },
+        body: JSON.stringify(post),
+      });
+
+      const res = await json.json();
+
+      if (Array.isArray(res)) {
+        console.log(res);
+        alert("Something is wrong with the data, check console");
+      } else {
+        navigate(`/${res.id}`);
+      }
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    function checkUpdate() {
-
-        const newPost = {
-            ...post,
-            published: !post.published
-        }
-        setPost(newPost)
-    }
-
-    async function formSubmit(e) {
-        e.preventDefault()
-
-        try {
-            const json = await fetch('http://localhost:3000/posts', { 
-                method: postToEdit ? 'PUT' : 'POST', 
-                headers: {"Content-Type": "application/json", "Authorization": `Bearer ${JSON.parse(localStorage.getItem("token"))}`},
-                body: JSON.stringify(post)
-            })
-
-            const res = await json.json()
-
-
-            if (Array.isArray(res)) {
-                console.log(res)
-                alert("Something is wrong with the data, check console")
-            } else {
-                navigate(`/${res._id}`)
-            }
-        } catch (err) {
-            console.log(err)
-        }
-
-    }
-
-    return (
-        <div className={style.formHolder}>
-            <form onSubmit={formSubmit}>
-                <div className={style.divForm}>
-                    <label htmlFor="title">Post title</label>
-                    <input onChange={formUpdate} type="text" id="title" name="title" value={post.title}/>
-                </div>
-                <div className={style.divForm}>
-                    <label htmlFor="text">Post text</label>
-                    <textarea onChange={formUpdate} name="text" id="text" value={post.text}></textarea>
-                </div>
-                <div className={style.checkboxDiv}>
-                    <input onChange={checkUpdate} type="checkbox" name="published" id="published" checked={post.published}/>
-                    <label htmlFor="published">Publish</label>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+  return (
+    <div className={style.formHolder}>
+      <form onSubmit={formSubmit}>
+        <div className={style.divForm}>
+          <label htmlFor="title">Post title</label>
+          <input
+            onChange={formUpdate}
+            type="text"
+            id="title"
+            name="title"
+            value={post.title}
+          />
         </div>
-    )
+        <div className={style.divForm}>
+          <label htmlFor="text">Post text</label>
+          <textarea
+            onChange={formUpdate}
+            name="text"
+            id="text"
+            value={post.text}
+          ></textarea>
+        </div>
+        <div className={style.checkboxDiv}>
+          <input
+            onChange={checkUpdate}
+            type="checkbox"
+            name="published"
+            id="published"
+            checked={post.published}
+          />
+          <label htmlFor="published">Publish</label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 CreatePost.propTypes = {
-    postToEdit: PropTypes.object
-}
+  postToEdit: PropTypes.object,
+};
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,30 +1,45 @@
 import { Link, useLoaderData } from "react-router-dom";
-import style from './header.module.css'
+import style from "./header.module.css";
 
-export default function Header () {
-    const user = useLoaderData()[0]
-    
-    if (!user) {
-        return (
-            <header>
-                <nav>
-                    <span className={style.home}><Link to="/">Home</Link></span>
-                    <Link to="/sign-up">Sign Up</Link>
-                    <Link to="/log-in">Log in</Link>
-                </nav>
-            </header>
-        )
-    } else {
-        return (
-            <header>
-                <nav>
-                    <span className={style.home}><Link to="/">Home</Link></span>
-                    {user.author && <span><Link to="/new-post">New Post</Link></span>}
-                    <span><Link to={"/user/"+ user._id}>{user.username}</Link></span>
-                    <Link onClick={()=> {localStorage.removeItem("token")}}>Log out</Link>
-                </nav>
-            </header>
-        )
-    }
+export default function Header() {
+  const user = useLoaderData()[0];
 
+  if (!user) {
+    return (
+      <header>
+        <nav>
+          <span className={style.home}>
+            <Link to="/">Home</Link>
+          </span>
+          <Link to="/sign-up">Sign Up</Link>
+          <Link to="/log-in">Log in</Link>
+        </nav>
+      </header>
+    );
+  } else {
+    return (
+      <header>
+        <nav>
+          <span className={style.home}>
+            <Link to="/">Home</Link>
+          </span>
+          {user.author && (
+            <span>
+              <Link to="/new-post">New Post</Link>
+            </span>
+          )}
+          <span>
+            <Link to={"/user/" + user.id}>{user.username}</Link>
+          </span>
+          <Link
+            onClick={() => {
+              localStorage.removeItem("token");
+            }}
+          >
+            Log out
+          </Link>
+        </nav>
+      </header>
+    );
+  }
 }
