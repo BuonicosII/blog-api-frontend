7b310aa8378e04a058cfbb58e16033236a2d195a
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2025-03-16 12:27:32 +0100

    Add styling for log in and sign up forms

--- src/components/log-in/log-in.jsx
+++ src/components/log-in/log-in.jsx
@@ -1,62 +1,77 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import style from "./log-in.module.css"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "./log-in.module.css";
 
-export default function LogIn () {
+export default function LogIn() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
 
-    const navigate = useNavigate()
-    const [user, setUser] = useState({})
+  function formUpdate(e) {
+    e.preventDefault();
 
-    function formUpdate(e) {
-        
-        e.preventDefault()
+    const newUser = {
+      ...user,
+      password: document.querySelector("#password").value,
+      username: document.querySelector("#username").value,
+    };
+    setUser(newUser);
+  }
 
-        const newUser = {
-            ...user,
-            password: document.querySelector("#password").value,
-            username: document.querySelector("#username").value
-        }
-        setUser(newUser)
+  async function formSubmit(e) {
+    e.preventDefault();
+    try {
+      const json = await fetch("http://localhost:3000/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      });
+      const res = await json.json();
 
+      if (typeof res === "string") {
+        console.log(res);
+        alert("Something is wrong with the data, check console");
+      } else {
+        localStorage.setItem("token", JSON.stringify(res.token));
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    async function formSubmit(e) {
-        e.preventDefault()
-        try {
-            const json = await fetch('http://localhost:3000/users/login', { 
-                method: 'POST', 
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(user)
-            })
-            const res = await json.json()
-
-            if (typeof res === "string") {
-                console.log(res)
-                alert("Something is wrong with the data, check console")
-            } else {
-                localStorage.setItem("token", JSON.stringify(res.token))
-                navigate("/")
-            }
-        } catch (err) {
-            console.log(err)
-        }
-
-    }
-
-    return (
+  return (
+    <main>
+      <div className={style.postFeed}>
         <div className={style.formHolder}>
-            <form onSubmit={formSubmit}>
-                <div className={style.divForm}>
-                    <label htmlFor="username">Username</label>
-                    <input onChange={formUpdate} type="text" name="username" id="username" value={user.username}/>
-                </div>
-                <div className={style.divForm}>
-                    <label htmlFor="password">Password</label>
-                    <input onChange={formUpdate} type="password" id="password" name="password" value={user.password}/>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+          <form onSubmit={formSubmit}>
+            <div className={style.divForm}>
+              <label htmlFor="username">Username</label>
+              <input
+                onChange={formUpdate}
+                type="text"
+                name="username"
+                id="username"
+                value={user.username}
+                className={style.formInput}
+              />
+            </div>
+            <div className={style.divForm}>
+              <label htmlFor="password">Password</label>
+              <input
+                onChange={formUpdate}
+                type="password"
+                id="password"
+                name="password"
+                value={user.password}
+                className={style.formInput}
+              />
+            </div>
+            <button type="submit" className={style.confirm}>
+              Submit
+            </button>
+          </form>
         </div>
-
-    )
+      </div>
+    </main>
+  );
 }
--- src/components/sign-up-form/sign-up-form.jsx
+++ src/components/sign-up-form/sign-up-form.jsx
@@ -1,81 +1,125 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import style from "./sign-up.module.css"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "./sign-up.module.css";
 
-export default function SignUp () {
+export default function SignUp() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
 
-    const navigate = useNavigate()
-    const [user, setUser] = useState({})
+  function formUpdate(e) {
+    e.preventDefault();
 
-    function formUpdate(e) {
-        
-        e.preventDefault()
+    const newUser = {
+      ...user,
+      firstName: document.querySelector("#firstName").value,
+      lastName: document.querySelector("#lastName").value,
+      email: document.querySelector("#email").value,
+      password: document.querySelector("#password").value,
+      passwordConfirm: document.querySelector("#passwordConfirm").value,
+      username: document.querySelector("#username").value,
+    };
+    setUser(newUser);
+  }
 
-        const newUser = {
-            ...user,
-            firstName: document.querySelector("#firstName").value, 
-            lastName: document.querySelector("#lastName").value,
-            email: document.querySelector("#email").value,
-            password: document.querySelector("#password").value,
-            passwordConfirm: document.querySelector("#passwordConfirm").value,
-            username: document.querySelector("#username").value
-        }
-        setUser(newUser)
+  async function formSubmit(e) {
+    e.preventDefault();
+    try {
+      const json = await fetch("http://localhost:3000/users/sign-up", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      });
 
+      const res = await json.json();
+
+      if (Array.isArray(res)) {
+        console.log(res);
+        alert("Something is wrong with the data, check console");
+      } else {
+        navigate("/log-in");
+      }
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    async function formSubmit(e) {
-        e.preventDefault()
-        try {
-            const json = await fetch('http://localhost:3000/users/sign-up', { 
-                method: 'POST', 
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(user)
-            })
-
-            const res = await json.json()
-
-            if (Array.isArray(res)) {
-                console.log(res)
-                alert("Something is wrong with the data, check console")
-            } else {
-                navigate("/log-in")
-            }
-        } catch (err) {
-            console.log(err)
-        }
-
-    }
-
-    return (
+  return (
+    <main>
+      <div className={style.postFeed}>
         <div className={style.formHolder}>
-            <form onSubmit={formSubmit}>
-                <div className={style.divForm}>
-                    <label htmlFor="firstName">First Name</label>
-                    <input onChange={formUpdate} type="text" id="firstName" name="firstName" value={user.firstName}/>
-                </div>
-                <div className={style.divForm}>
-                    <label htmlFor="lastName">Last Name</label>
-                    <input onChange={formUpdate} type="text" id="lastName" name="lastName" value={user.lastName}/>
-                </div>
-                <div className={style.divForm}>
-                    <label htmlFor="email">E-mail</label>
-                    <input onChange={formUpdate} type="email" id="email" name="email" value={user.email}/>
-                </div>
-                <div className={style.divForm}>
-                    <label htmlFor="password">Password</label>
-                    <input onChange={formUpdate} type="password" id="password" name="password" value={user.password}/>
-                </div>
-                <div className={style.divForm}>
-                    <label htmlFor="passwordConfirm">Confirm password</label>
-                    <input onChange={formUpdate} type="password" name="passwordConfirm" id="passwordConfirm" value={user.passwordConfirm}/>
-                </div>
-                <div className={style.divForm}>
-                    <label htmlFor="username">Username</label>
-                    <input onChange={formUpdate} type="text" name="username" id="username" value={user.username}/>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+          <form onSubmit={formSubmit}>
+            <div className={style.divForm}>
+              <label htmlFor="firstName">First Name</label>
+              <input
+                onChange={formUpdate}
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={user.firstName}
+                className={style.formInput}
+              />
+            </div>
+            <div className={style.divForm}>
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                onChange={formUpdate}
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={user.lastName}
+                className={style.formInput}
+              />
+            </div>
+            <div className={style.divForm}>
+              <label htmlFor="email">E-mail</label>
+              <input
+                onChange={formUpdate}
+                type="email"
+                id="email"
+                name="email"
+                value={user.email}
+                className={style.formInput}
+              />
+            </div>
+            <div className={style.divForm}>
+              <label htmlFor="password">Password</label>
+              <input
+                onChange={formUpdate}
+                type="password"
+                id="password"
+                name="password"
+                value={user.password}
+                className={style.formInput}
+              />
+            </div>
+            <div className={style.divForm}>
+              <label htmlFor="passwordConfirm">Confirm password</label>
+              <input
+                onChange={formUpdate}
+                type="password"
+                name="passwordConfirm"
+                id="passwordConfirm"
+                value={user.passwordConfirm}
+                className={style.formInput}
+              />
+            </div>
+            <div className={style.divForm}>
+              <label htmlFor="username">Username</label>
+              <input
+                onChange={formUpdate}
+                type="text"
+                name="username"
+                id="username"
+                value={user.username}
+                className={style.formInput}
+              />
+            </div>
+            <button type="submit" className={style.confirm}>
+              Submit
+            </button>
+          </form>
         </div>
-    )
+      </div>
+    </main>
+  );
 }
